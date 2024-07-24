7d3018c8ef4bad9dceedef4632139729479e5278
muhammed shinadh <muhammedshinad2@gmail.com>
2024-07-24 20:11:41 +0530

    Doing AdminDashboard

---
+++ frontend/src/Components/Admin/AdminDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AdminDashboard() {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  )
+}
+
+export default AdminDashboard
--- frontend/src/Routes/AdminRoute.jsx
+++ frontend/src/Routes/AdminRoute.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import LoginPage from '../Pages/AdminPages/LoginPage';
 import { Routes, Route } from "react-router-dom";
+import AdminDashboard from '../Pages/AdminPages/DashboardPage';
 
 function UserRoute() {
     return (
         <div>
             <Routes>
             <Route path='/' element={<LoginPage />} />
+            <Route path='/dashboard' element={<AdminDashboard/>} />
             </Routes>
         </div >
     )
